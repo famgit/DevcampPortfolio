57384cd57e78936372f8c1327a38cf0af383fb20
famgit <tekbaz2000@gmail.com>
2020-06-01 05:51:16 -0700

    Implemented validations for blogs, skills, and portfolio items

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
  get 'about-us', to: 'pages#about'     # get 'pages/about'
   get 'contact-us', to: 'pages#contact'# get 'pages/contact'
 
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
   root to: 'pages#home'
 end
