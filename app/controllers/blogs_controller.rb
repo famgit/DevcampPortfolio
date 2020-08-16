4a490d65746dc91aa964a73f0b015689cba66023
famgit <tekbaz2000@gmail.com>
2020-08-15 18:38:33 -0700

    Added radio buttons for status field for blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -93,7 +93,7 @@ redirect_to blogs_url, notice: 'Post status has been updated'
 
     # Only allow a list of trusted parameters through.
     def blog_params
-      params.require(:blog).permit(:title, :body, :topic_id)
+      params.require(:blog).permit(:title, :body, :topic_id,:status)
     end
   def set_sidebar_topics
     @side_bar_topics = Topic.with_blogs
