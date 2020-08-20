5ea0cfa084c8f1adf244d0e26e697fcfaa3d664b
famgit <tekbaz2000@gmail.com>
2020-08-19 18:35:10 -0700

    Implemented sorce-tracking feature

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :portfolios, except:[:show,:edit]  do
     put :sort, on: :collection
   end
+  get 'tech_news', to: 'pages#tech_news'
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'portfolio/:id/edit', to: 'portfolios#edit', as: 'edit_portfolio'
